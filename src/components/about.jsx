54f6ee7e962486576e69bf5ec06849c42e356cc5
AFeese <58886725+AFeese@users.noreply.github.com>
2020-08-02 21:33:30 -0400

    updated site photo in about me section. Updated skill set.

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import myImage from "../img/MERN.png";
+import myImage from "../img/myImage.png";
 
-class About extends React.Component{
-    constructor(){
+class About extends React.Component {
+    constructor() {
         super();
         this.state = {
-            skills:[
-                {id:"HTML5_skill", content: "HTML5",percentage: "90%", value: "90"},
-                {id:"CSS3_skill", content: "CSS",percentage: "75%", value: "75"},
-                {id:"JavaScript_skill", content: "JavaScript",percentage: "90%", value: "90"},
-                // {id:"PHP_skill", content: "PHP",percentage: "70%", value: "70"},
-                {id:"ReactJS_skill", content: "ReactJS",percentage: "80%", value: "80"},
-                // {id:"Python_skill", content: "Python",percentage: "75%", value: "75"},
+            skills: [
+                { id: "HTML5_skill", content: "HTML5", percentage: "90%", value: "90" },
+                { id: "CSS_skill", content: "CSS", percentage: "90%", value: "90" },
+                { id: "JavaScript_skill", content: "JavaScript", percentage: "90%", value: "90" },
+                { id: "ReactJS_skill", content: "ReactJS", percentage: "60%", value: "80" },
+                { id: "Backend_server_skill", content: "Backend | Server", percentage: "75%", value: "75" },
+                { id: "Digital_logo_design_skill", content: "Digital Logo Design", percentage: "95%", value: "95" },
                 // {id:"VanillaJS_skill", content: "VanillaJS",percentage: "85%", value: "85"},
                 // {id:"Wordpress_skill", content: "Wordpress",percentage: "80%", value: "80"}
             ],
-            about_me:[
-                {id: "first-p-about",content: "A former hospitality enthusiast turned ardent and creative developer - "},
-                {id: "second-p-about", content: "Motivated by intellectual curiosity, developing software, team collaboration, solving challenges, and now has a new-found love for programming and design."},
-                {id: "third-p-about", content: "How can I help you and your team?"}
+            about_me: [
+                { id: "first-p-about", content: "A former hospitality enthusiast turned ardent and creative developer - " },
+                { id: "second-p-about", content: "Motivated by intellectual curiosity, developing software, team collaboration, solving challenges, and now has a new-found love for programming and design." },
+                { id: "third-p-about", content: "How can I help you and your team?" }
             ]
         }
     }
 
-    render(){
+    render() {
         return (
             <section id="about" className="about-mf sect-pt4 route aboutContainer">
                 <div className="container">
@@ -33,33 +33,34 @@ class About extends React.Component{
                                 <div className="row">
                                     <div className="col-md-6">
                                         <div className="row">
-                                            <div className="col-sm-6 col-md-5" style={{margin: "0 auto"}}>
-                                                <div className="about-img" style={{textAlign: "center"}}>
-                                                    <img src={myImage} className="img-fluid rounded b-shadow-a" alt=""/>
+                                            {/* <div className="col-sm-6 col-md-5" style={{ margin: "0 auto" }}>
+                                                <div className="about-img" style={{ textAlign: "center" }}>
+                                                    <img src={myImage} className="img-fluid rounded b-shadow-a" alt="" />
                                                 </div>
-                                            </div>
+                                            </div> */}
                                         </div>
                                         <div className="skill-mf">
-                                            {/* <p className="title-s">Skill</p> */}
+                                            <p className="title-s">Skill</p>
                                             {
                                                 this.state.skills.map(skill => {
-                                                    return(
+                                                    return (
                                                         <React.Fragment key={skill.id}>
                                                             <span>{skill.content}</span> <span className="pull-right">{skill.percentage}</span>
                                                             <div className="progress">
-                                                                <div className="progress-bar" role="progressbar" style={{width: skill.percentage}} aria-valuenow={skill.value} aria-valuemin="0" aria-valuemax="100"></div>
+                                                                <div className="progress-bar" role="progressbar" style={{ width: skill.percentage }} aria-valuenow={skill.value} aria-valuemin="0" aria-valuemax="100"></div>
                                                             </div>
                                                         </React.Fragment>
                                                     );
                                                 })
                                             }
+
                                         </div>
                                     </div>
                                     <div className="col-md-6">
-                                        <div className="about-me pt-4 pt-md-0">
+                                        <div className="about-me-center pt-4 pt-md-0">
                                             <div className="title-box-2">
-                                                <h5 className="title-left">
-                                                About Me
+                                                <h5 className="about-title-center">
+                                                    About Me
                                                 </h5>
                                             </div>
                                             {
@@ -67,6 +68,11 @@ class About extends React.Component{
                                                     return <p className="lead" key={content.id}>{content.content}</p>;
                                                 })
                                             }
+                                        </div>
+                                        <div className="col-sm-6 col-md-5" style={{ margin: "0 auto" }}>
+                                            <div className="about-img" style={{ textAlign: "center" }}>
+                                                <img src={myImage} className="img-fluid rounded b-shadow-a" alt="" />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>

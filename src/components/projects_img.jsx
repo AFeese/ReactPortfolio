a32886e6ac659e6edef62fba3f5eeb886452d1dd
AFeese <58886725+AFeese@users.noreply.github.com>
2020-11-11 12:47:30 -0500

    cleaned up code and removed unused items in project.

--- src/components/projects_img.jsx
+++ src/components/projects_img.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function ProjectImg(props) {
-
-    return (
-        <div>
-            <img src={props.image} alt="projects" className="responsive projectsIcon"/>
-        </div>
-    )
-};
+  return (
+    <div>
+      <img
+        src={props.image}
+        alt="projects"
+        className="responsive projectsIcon"
+      />
+    </div>
+  );
+}
 
 export default ProjectImg;
